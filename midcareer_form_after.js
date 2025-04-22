6fa7dda3a5c5faf2024fd0d06ec49d6334bb603e
MUBA-A <92009486+MUBA-A@users.noreply.github.com>
2025-04-22 22:26:08 +0900

    Update midcareer_form_after.js

--- midcareer_form_after.js
+++ midcareer_form_after.js
@@ -438,11 +438,12 @@
     const privacyPolicyTimestampField = shadow.getElementById('entry_privacyPolicyTimestamp');
     const desiredOccupation = shadow.getElementById('entry_desiredOccupation');
 
+    let occupation = "その他";
     if (window.location.search != "") {
         const params = new URLSearchParams(window.location.search);
-        const referrer = params.get("occupation").toLowerCase();
+        const referrer = params.get("occupation", "").toLowerCase();
 
-        let occupation = "その他";
+        
         const occupations = {
             "is": "インサイドセールス",
             "fs": "フィールドセールス",
