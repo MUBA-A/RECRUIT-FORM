bf5c008c75f16c15a1c90f5aed0575461963b628
MUBA-A <92009486+MUBA-A@users.noreply.github.com>
2025-04-29 22:52:07 +0900

    Update newgrad_form_after.js

--- newgrad_form_after.js
+++ newgrad_form_after.js
@@ -265,25 +265,26 @@
       // Add media query for tablet
         const mediaQuery = document.createElement('style');
         mediaQuery.textContent = `
-             @media (min-width: 1501px) {
+             @media (min-width: 1139px) {
                 #entry_form-container {
-                    width: 100%;
                     max-width: 600px;
+                    width: 100%;
                 }
             }
-            @media (min-width: 1240px) and (max-width: 1500px) {
-                #entry_form-container {
-                    width: 80%;
-                }
-            }
-            @media (min-width: 704px) and (max-width: 1239px) {
+            @media (min-width: 704px) and (max-width: 1139px) {
                 #entry_form-container {
                     width: 100%;
+                }
+                #form_text {
+                    font-size: 16px;
                 }
             }
             @media (max-width: 704px) and (min-width: 541px) {
                 #entry_form-container {
                     width: 300px;
+                }
+                #form_text {
+                    font-size: 12px;
                 }
             }
             @media (max-width: 540px) {
@@ -359,7 +360,7 @@
             <div class="form-row">
                 <div class="form-group">
                     <label for="entry_school" class="required-label">学校名</label>
-                    <input type="text" id="entry_school" name="school" required aria-required="true" placeholder="○○大学">
+                    <input type="text" id="entry_school" name="school" required aria-required="true" placeholder="○○大学、○○大学院">
                     <div class="error-message" id="entry_schoolError">学校名を入力してください</div>
                 </div>
                 <div class="form-group">
