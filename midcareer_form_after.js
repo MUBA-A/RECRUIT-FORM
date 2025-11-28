8b98cc94459da3274f46dcf43c0b4b3c9195d898
MUBA-A <92009486+MUBA-A@users.noreply.github.com>
2025-11-28 14:52:29 +0900

    Update midcareer_form_after.js

--- midcareer_form_after.js
+++ midcareer_form_after.js
@@ -555,7 +555,15 @@
                 formData.set("desiredOccupation", occupation);
             }
 
-            
+            for (const [key, value] of [...formData.entries()]) {
+                 if (value instanceof File) {
+                     // Create safe name
+                     const safeName = `upload-${Date.now()}.${value.name.split('.').pop()}`;
+                     
+                     // Overwrite the file in the existing formData with the renamed version
+                     formData.set(key, new File([value], safeName, { type: value.type }));
+                 }
+             }
             
             setFormSubmitting(true);
 
