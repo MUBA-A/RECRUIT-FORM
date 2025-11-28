5a42ee13b06df7f64979f69bb09e85bec7d1c340
MUBA-A <92009486+MUBA-A@users.noreply.github.com>
2025-11-28 16:33:00 +0900

    Update midcareer_form_after.js

--- midcareer_form_after.js
+++ midcareer_form_after.js
@@ -612,7 +612,7 @@
                     'Phone': formData.get('phone'),
                     'graduation': formData.get('graduationYear'),
                     'praivacyPolicy': formData.get('privacyPolicy') !== null ? "yes" : "no",
-                    'recordtype': '応募者_新卒'
+                    'recordtype': '応募者_中途'
                 });
                 
                 // Submit Marketo form (Fire and forget)
