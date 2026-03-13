7bafa619cba3aa1abbc3c414403c9b855c013766
MUBA-A <92009486+MUBA-A@users.noreply.github.com>
2026-03-13 18:38:10 +0900

    Google tag for submit button click

--- HP_form.js
+++ HP_form.js
@@ -580,6 +580,14 @@
         }
         
         if (isValid) {
+
+           // This tells GA4: "Someone clicked the submit button!"
+             if (typeof gtag === 'function') {
+                 gtag('event', 'form_submit_attempt', {
+                     'event_category': 'Application',
+                     'event_label': 'New Grad Form'
+                 });
+             }
             // Get form data
             const formData = new FormData(form);
             for (const [key, value] of [...formData.entries()]) {
