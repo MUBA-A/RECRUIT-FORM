31b86defc25f8f174a5a2ab8ffbe400b0f9de7e9
MUBA-A <92009486+MUBA-A@users.noreply.github.com>
2026-03-30 19:11:30 +0900

    Update test.js

--- test.js
+++ test.js
@@ -369,8 +369,6 @@
                     </div>
                     <div class="error-message" id="entry_CVError">職務経歴書をアップロードしてください（PDF、Excel、Word形式、10MB以下）</div>
                 </div>
-                <div class="form-group">
-                </div>
 
                 <div class="form-group">
                     <label for="entry_CV" class="required-label">応募媒体</label>
